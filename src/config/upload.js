d3cfe4a15378bfab4aa7d2d2235a8195f7bd3c19
Victor Paranhos <victor_paranhos@hotmail.com>
2023-09-08 15:50:02 -0300

    diskstorage

--- src/config/upload.js
+++ src/config/upload.js
@@ -6,10 +6,17 @@ const TMP = path.resolve(__dirname, "..", "..", "tmp")
 const UPLOADS = path.resolve(TMP, "uploads")
 
 const MULTER = {
-    storage: multer.diskStorage({
-        destination: TMP,
-        filename(request, file, callback){
-         
-        }
-    })
+   storage: multer.diskStorage({
+    destination: TMP,
+    filename(request, file, callback){
+        const fileHashed = crypto.randomBytes(8).toString("hex")
+        const fileName = `${fileHashed}-${file.originalname}`
+        return callback(null, fileName)
+    }
+
+   })
+}
+
+module.exports = {
+    TMP, UPLOADS, MULTER
 }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,12 +2,14 @@ const { Router } = require("express")
 const UserControllers = require("../controllers/userControllers")
 const userControllers = new UserControllers()
 const ensureAuthentication = require("../middleware/ensureAuthentication")
+const UsersAvatarController = require("../controllers/userAvatarController");
+const userAvatarController = new UsersAvatarController()
 const userRoutes = Router()
 
 userRoutes.post("/", userControllers.create)
 userRoutes.put("/", ensureAuthentication, userControllers.update)
 userRoutes.delete("/", ensureAuthentication, userControllers.delete)
-
+userRoutes.patch("/avatar", ensureAuthentication, userAvatarController.create)
 
 
 module.exports = userRoutes;
